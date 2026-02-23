ef61373744d54e24387d821cda954007b5219fdb
Hiếu GL Lite <100750590+ShilukaYT@users.noreply.github.com>
2026-02-24 01:29:11 +0700

    Updated

--- Webview-Frontend/vite.config.js
+++ Webview-Frontend/vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/HieuGLLite.App/Webview-Frontend/',
+  base: './',
   
   plugins: [
     vue(),
